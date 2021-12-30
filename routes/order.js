d382a6c6f21cea802ce87f358e8474f3450a6daf
mianabdullah520 <95604816+mianabdullah520@users.noreply.github.com>
2021-12-30 18:14:14 +0500

    Correction in order.js

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Gig = require('../models/gig');
 const User = require('../models/user');
+const stripe = require('stripe')('sk_test_VBo5DZeMx19wmpLPcJ49esfs');
 const Order = require('../models/order');
 const fee = 3.15;
 const async = require('async');
@@ -52,6 +53,131 @@ router.get('/checkout/process_cart', (req, res) => {
                 totalPrice: totalPrice,
                 sub_total: price,
                 cartIsEmpty: cartIsEmpty
+            });
+        });
+});
+
+//Single payment
+router
+    .route('/payment')
+    .get((req, res, next) => {
+        res.render('checkout/payment');
+    })
+    .post((req, res, next) => {
+        var gig = req.session.gig;
+        var price = req.session.price;
+        price *= 100;
+        stripe.customers
+            .create({ email: req.user.email })
+            .then(customer => {
+                return stripe.customers.createSource(customer.id, {
+                    source: req.body.stripeToken
+                });
+            })
+            .then(source => {
+                return stripe.charges.create({
+                    amount: price,
+                    currency: 'usd',
+                    customer: source.customer
+                });
+            })
+            .then(charge => {
+                var order = new Order();
+                order.buyer = req.user._id;
+                order.seller = gig.owner;
+                order.gig = gig._id;
+                order.save(err => {
+                    req.session.gig = null;
+                    req.session.price = null;
+                    res.redirect(
+                        '/users/' + req.user._id + '/orders/' + order._id
+                    );
+                });
+            })
+            .catch(err => {
+                // Deal with an error
+            });
+    });
+
+//Handle payment through cart
+router
+    .route('/payment/cart')
+    .get((req, res, next) => {
+        res.render('checkout/payment');
+    })
+    .post((req, res, next) => {
+        var gigs = req.session.gig;
+        var price = req.session.price;
+        price *= 100;
+        stripe.customers
+            .create({ email: req.user.email })
+            .then(function(customer) {
+                return stripe.customers.createSource(customer.id, {
+                    source: req.body.stripeToken
+                });
+            })
+            .then(function(source) {
+                return stripe.charges.create({
+                    amount: price,
+                    currency: 'usd',
+                    customer: source.customer
+                });
+            })
+            .then(function(charge) {
+                // DO SOMETHING
+                gigs.map(function(gig) {
+                    var order = new Order();
+                    order.buyer = req.user._id;
+                    order.seller = gig.owner;
+                    order.gig = gig._id;
+                    order.save(function(err) {
+                        req.session.gig = null;
+                        req.session.price = null;
+                    });
+                });
+                User.update(
+                    {
+                        _id: req.user._id
+                    },
+                    {
+                        $set: {
+                            cart: []
+                        }
+                    },
+                    function(err, updated) {
+                        if (updated) {
+                            res.redirect('/users/' + req.user._id + '/orders');
+                        }
+                    }
+                );
+            })
+            .catch(function(err) {
+                // Deal with an error
+            });
+    });
+
+//Chat page
+router.get('/users/:userId/orders/:orderId', (req, res, next) => {
+    req.session.orderId = req.params.orderId;
+    Order.findOne({ _id: req.params.orderId })
+        .populate('buyer')
+        .populate('seller')
+        .populate('gig')
+        .deepPopulate('messages.owner')
+        .exec(function(err, order) {
+            console.log(order);
+            res.render('order/order-room', {
+                layout: 'chat-layout',
+                order: order,
+                helpers: {
+                    if_equals: function(a, b, opts) {
+                        if (a.equals(b)) {
+                            return opts.fn(this);
+                        } else {
+                            return opts.inverse(this);
+                        }
+                    }
+                }
             });
         });
 });
